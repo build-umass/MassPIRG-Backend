2323a1a4927720e54500bb131a2bc728cd5e6bf0
ThomasN12 <phuocthinhdn12023@gmail.com>
2023-01-24 13:02:29 -0500

    Update eBoardMember model comment

--- models/eBoardMember.js
+++ models/eBoardMember.js
@@ -2,12 +2,12 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const eBoardMemberSchema = new Schema({
-    name: String,
-    role: String,
-    class: Number,
-    major: String,
-    email: String,
-    description: String,
+    name: String,   // Ashley Agostinell
+    role: String,   // Chapter Chair
+    class: Number,  // 2023
+    major: String,  // Psychology and Linguistics Major
+    email: String,  // amagostinell@umass.edu
+    description: String, // Ashley is the Chair of MASSPIRG Students...
 })
 
 const EBoardMember = mongoose.model('EBoardMember', eBoardMemberSchema);
