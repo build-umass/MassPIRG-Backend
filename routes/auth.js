2f94801414e4609f09a61fa02acf6719a5a7229c
ThomasN12 <phuocthinhdn12023@gmail.com>
2023-05-08 20:05:25 -0400

    lock register api

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { validateUserRegister, validateUserLogin } from '../middlewares/middlewares.js';
+import { validateUserRegister, validateUserLogin, verifyToken } from '../middlewares/middlewares.js';
 import { loginUser, registerUser } from '../controllers/authController.js';
 
 const router = express.Router();
-router.post('/register', validateUserRegister, registerUser)
+router.post('/register', validateUserRegister, verifyToken, registerUser);
 
-router.post('/login', validateUserLogin, loginUser)
+router.post('/login', validateUserLogin, loginUser);
 
 // router.post('/googlelogin', googleLoginUser);
 // router.post('/refresh', requestRefreshToken);
